330b6524a21c933410465fe7d17965c4dfa8ddeb
Emmanuel Dedryver <marcossuarezr88@gmail.com>
2023-03-07 21:52:14 +0100

    devise views translated

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  before_action :authenticate_user!, :set_locale
+  # before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+
+  def set_locale
+    I18n.locale = :fr
+  end
 end
