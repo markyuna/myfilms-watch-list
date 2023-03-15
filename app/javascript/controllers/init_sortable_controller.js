eacd23c57ef78f07df880dc79ad7bbafc204cd6e
Emmanuel Dedryver <marcossuarezr88@gmail.com>
2023-03-15 01:31:03 +0100

    insert movies controller

--- app/javascript/controllers/init_sortable_controller.js
+++ app/javascript/controllers/init_sortable_controller.js
@@ -4,7 +4,13 @@ import Sortable from "sortablejs"
 // Export initSortable function
 export const initSortable = () => {
   const list = document.querySelector("#results")
-  Sortable.create(list)
+  Sortable.create(list, {
+    ghostClass: "ghost",
+    animation: 150,
+    // onEnd: (event) => {
+    //   alert(`${event.oldIndex} moved to ${event.newIndex}`)
+    // }
+  })
 }
 
 // Connects to data-controller="init-sortable"
